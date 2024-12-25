08c6a0ea9eb3a43542623834a35593993d2d5d76
HaranK007 <shreeharan2003@gmail.com>
2024-12-25 17:28:14 +0530

    architecture upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
-import { ConsolePage } from './pages/ConsolePage';
-import './App.scss';
+import { useEffect, useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+import Loader from './common/Loader';
+import PageTitle from './components/PageTitle';
+import Conversaction from './pages/Conversation';
+import Profile from './pages/Profile';
+import DefaultLayout from './layout/DefaultLayout';
 
 function App() {
-  return (
-    <div data-component="App">
-      <ConsolePage />
-    </div>
+  const [loading, setLoading] = useState<boolean>(true);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <DefaultLayout>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <PageTitle title="Home" />
+              <Conversaction />
+            </>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <>
+              <PageTitle title="Profile | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <Profile />
+            </>
+          }
+        />
+      </Routes>
+    </DefaultLayout>
   );
 }
 
