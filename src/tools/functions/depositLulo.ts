9228975eee6c1c4ea996f6013ca4be55985cfdee
HaranK007 <shreeharan2003@gmail.com>
2025-01-06 11:13:22 +0530

    perf: Perfomance improvement

--- src/tools/functions/depositLulo.ts
+++ src/tools/functions/depositLulo.ts
@@ -2,28 +2,26 @@ const functionDescription =
   'Call this function when the user wants to deposit stable coins to Lulo.';
 
 export const depositLulo = {
-    type: 'function',
-    name: 'depositLulo',
-    description: functionDescription,
-    parameters: {
-        type: 'object',
-        strict: true,
-        properties: {
-            amount: {
-                type: 'number',
-                description: 'Amount of stable coin to deposit.',
-            },
-            token: {
-                type: 'string',
-                enum: ['USDT', 'USDS', 'USDC'],
-                description: 'The stable coin to deposit.',
-            },
-        }
+  type: 'function',
+  name: 'depositLulo',
+  description: functionDescription,
+  parameters: {
+    type: 'object',
+    strict: true,
+    properties: {
+      amount: {
+        type: 'number',
+        description: 'Amount of stable coin that the user wants to deoposit.',
+      },
+      token: {
+        type: 'string',
+        enum: ['USDT', 'USDS', 'USDC'],
+        description: 'The symbol/name of the coin user wants to deposit.',
+      },
     },
+    required: ['amount', 'token'],
+  },
 };
 
-
 //TODO: Shift the trigger logic here from conversation.tsx
-export function depositLuloFunction() {
-  
-}
+export function depositLuloFunction() {}
