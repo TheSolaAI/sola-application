e9aa633b41190b8b747cac3ee495c3001a369838
HaranK007 <shreeharan2003@gmail.com>
2025-01-06 13:30:19 +0530

    ui: images background fix-2

--- src/pages/Conversation.tsx
+++ src/pages/Conversation.tsx
@@ -31,26 +31,7 @@ const Conversation = () => {
   const [isWalletVisible, setIsWalletVisible] = useState(false);
   const [events, setEvents] = useState<any[]>([]);
   const audioElement = useRef<HTMLAudioElement | null>(null);
-  const [messageList, setMessageList] = useState<MessageCard[]>([{
-    type: 'luloCard',
-    card: {
-      depositValue: 100,
-      interestEarned: 10,
-      totalValue: 110,
-      tokenBalance: [
-        {
-          balance: 100,
-          mint: 'USDSwr9ApdHk5bvJKMjzff41FfuX8bSxdKcR81vTwcA',
-          usdValue: 100,
-        },
-        {
-          balance: 100,
-          mint: '0xTokenAddress2',
-          usdValue: 100,
-        },
-      ],
-    },
-  } ]);
+  const [messageList, setMessageList] = useState<MessageCard[]>();
   const [luloTotal, setLuloTotal] = useState(0);
 
   const { appWallet } = useAppState();
