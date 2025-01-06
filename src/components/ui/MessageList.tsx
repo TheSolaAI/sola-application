5c849ae5efa5a4b94138c543952fa3073cbbca00
HaranK007 <shreeharan2003@gmail.com>
2025-01-06 13:08:04 +0530

    ui: lulo card updates

--- src/components/ui/MessageList.tsx
+++ src/components/ui/MessageList.tsx
@@ -118,7 +118,7 @@ const MessageList: React.FC<Props> = ({ messageList }) => {
                       <img
                         src={token.image || '/placeholder.png'}
                         alt={token.metadata?.name || 'Token'}
-                        className="h-10 w-10 rounded-lg bg-gray-200"
+                        className="h-10 w-10 rounded-lg  bg-graydark"
                       />
                       <div>
                         <h3 className="truncate text-sm font-medium">
@@ -142,32 +142,48 @@ const MessageList: React.FC<Props> = ({ messageList }) => {
             const tokenBalance = lulo.tokenBalance;
             return (
               <>
-                <div className="mb-4 bg-[#F5F5F5] p-3 rounded-lg leading-relaxed overflow-auto no-scrollbar transition-opacity duration-500 opacity-100 transform">
-                  <p className='text-base font-medium'>Deposit Value : {lulo.depositValue}</p>
-                  <p className='text-sm'>Interest Earned : {lulo.interestEarned}</p>
-                  <p className='text-sm'>Total Value : {lulo.totalValue}</p>
+                <div className="mb-4 bg-[#F5F5F5] flex flex-row gap-4 p-3 rounded-lg leading-relaxed overflow-auto no-scrollbar transition-opacity duration-500 opacity-100 transform">
+                  <img
+                    src="/lulo.jpg"
+                    alt="luloimage"
+                    className="h-16 rounded-sm"
+                  />
+                  <div>
+                    <p className="text-base font-medium">
+                      Deposit Value : {lulo.depositValue}
+                    </p>
+                    <p className="text-sm">
+                      Interest Earned : {lulo.interestEarned}
+                    </p>
+                    <p className="text-sm">Total Value : {lulo.totalValue}</p>
+                  </div>
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4 my-4">
                   {tokenBalance.map((token, tokenIndex) => (
                     <a
                       key={tokenIndex}
                       href={`https://dexscreener.com/solana/${token.mint}`}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="group relative block rounded-xl bg-[#F5F5F5] border p-3"
+                      className="group relative w-full overflow-hidden block rounded-xl bg-[#F5F5F5] border p-3"
                     >
                       <div className="flex items-center gap-3">
+                        <img
+                          src={`/${token.mint}.jpg` || '/placeholder.png'}
+                          alt="Token"
+                          className="h-10 w-10 rounded-lg bg-gray-200"
+                        />
                         <div>
                           <h3 className="truncate text-sm font-medium">
                             Balance : {token.balance || 'Unknown'}
                           </h3>
                           <p className={`mt-1 text-xs font-medium`}>
                             Mint : {token.mint.substring(0, 3)}...
-                            {token.mint.substring(-1, -4)}
+                            {token.mint.slice(-3)}
                           </p>
                         </div>
                       </div>
-                      <p className="text-xs text-gray-500">
+                      <p className="text-xs text-gray-500 mt-2">
                         Value : {token.usdValue}
                       </p>
                     </a>
