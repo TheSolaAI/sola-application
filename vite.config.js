d2a71649e79a1d6814d0a696341dd08972704a19
Ashwanth Balakrishnan <ashwanthbalakrishnan5@gmail.com>
2025-01-07 17:19:26 +0530

    feat: add dotenv for environment variable management and implement CI/CD workflow

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
+import 'dotenv/config';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
   return {
     build: {
       sourcemap: true,
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
       react(),
       nodePolyfills(),
       sentryVitePlugin({
-        authToken: process.env.SENTRY_AUTH_TOKEN,
+        authToken: process.env.VITE_SENTRY_AUTH_TOKEN,
         org: 'solaai',
         project: 'sola-application-frontend',
       }),
