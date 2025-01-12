7739b56e62f7036c6dcfc5754a24900a7a18a386
HaranK007 <shreeharan2003@gmail.com>
2025-01-13 00:18:03 +0530

    feat: Experiment to config embedded wallet

--- src/store/zustand/AppState.ts
+++ src/store/zustand/AppState.ts
@@ -5,9 +5,11 @@ interface AppState {
   authorized: boolean;
   appWallet: ConnectedSolanaWallet | null;
   appWallets: ConnectedSolanaWallet[];
+  embeddedWalletVisiblity: boolean;
   disclaimer: boolean;
   setAuthorized: (auth: boolean) => void;
   setWallet: (wallet: ConnectedSolanaWallet) => void;
+  setEmbeddedWalletVisiblity: (embeddedWalletVisiblity: boolean) => void;
   setDesclaimer: (disclaimer: boolean) => void;
 }
 
@@ -15,9 +17,11 @@ const appStateCreator: StateCreator<AppState> = (set) => ({
   authorized: false,
   appWallet: null,
   appWallets: [],
+  embeddedWalletVisiblity: false,
   disclaimer: true,
   setAuthorized: (authorized: boolean) => set({ authorized }),
   setWallet: (appWallet: ConnectedSolanaWallet) => set({ appWallet }),
+  setEmbeddedWalletVisiblity: (embeddedWalletVisiblity: boolean) => set({embeddedWalletVisiblity}),
   setDesclaimer: (disclaimer: boolean) => set({ disclaimer }),
 });
 
