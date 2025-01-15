9badc80cda94f0dcf84b929df0377477839810ca
Ashwanth Balakrishnan <ashwanthbalakrishnan5@gmail.com>
2025-01-16 00:31:37 +0530

    feat: add compression plugins and update deployment process for optimized asset delivery

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
+import viteCompression from 'vite-plugin-compression';
+// import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -18,10 +20,20 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       sourcemap: true,
+      minify: 'esbuild',
     },
     plugins: [
       react(),
       nodePolyfills(),
+      viteCompression({
+        algorithm: 'brotliCompress',
+      }),
+      viteCompression({
+        algorithm: 'gzip',
+      }),
+      // visualizer({
+      //   open: true, // Open visual report in the browser
+      // }),
       sentryVitePlugin({
         authToken: process.env.SENTRY_AUTH_TOKEN,
         org: 'solaai',
