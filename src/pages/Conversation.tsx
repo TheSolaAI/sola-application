9a42f60c1f1ac1b92da9151c434b6dd22079eb9b
HaranK007 <shreeharan2003@gmail.com>
2025-01-19 19:00:23 +0530

    perf: gpt model change

--- src/pages/Conversation.tsx
+++ src/pages/Conversation.tsx
@@ -1225,7 +1225,7 @@ const Conversation = () => {
       await pc.setLocalDescription(offer);
 
       const baseUrl = 'https://api.openai.com/v1/realtime';
-      const model = 'gpt-4o-realtime-preview-2024-12-17';
+      const model = 'gpt-4o-mini-realtime-preview-2024-12-17';
 
       const sdpResponse = await fetch(`${baseUrl}?model=${model}`, {
         method: 'POST',
