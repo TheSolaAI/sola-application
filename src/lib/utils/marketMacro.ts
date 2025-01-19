c77bcaf5c8571390e971619d5ffa59b0f9b5b56b
Toastx <toastx.sol@gmail.com>
2025-01-20 00:28:27 +0530

    feat: shift session to rs

--- src/lib/utils/marketMacro.ts
+++ src/lib/utils/marketMacro.ts
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 
-const url = process.env.PROXY_SERVER2_URL
+const url = process.env.DATA_SERVICE_URL;
 
 export const getMarketData = async () => {
     if (!url) { 
         return;
     }
-  let market_url = url + 'market';
+  let market_url = url + 'data/market/market';;
   const response = await axios.get(market_url);
   return response.data;
 };
