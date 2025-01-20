b745ff89ca4e883c0857bb76b57655763eca4886
HaranK007 <shreeharan2003@gmail.com>
2025-01-20 20:01:35 +0530

    feat: added configuration for blinks implementation

---
+++ src/hooks/useActionSolanaWalletAdapter.ts
@@ -0,0 +1,93 @@
+import {
+    ActionConfig,
+    createSignMessageText,
+    type SignMessageVerificationOptions,
+    verifySignMessageData,
+  } from '@dialectlabs/blinks-core';
+  import type { SignMessageData } from '@solana/actions-spec';
+  import { Connection, VersionedTransaction } from '@solana/web3.js';
+  import bs58 from 'bs58';
+  import { useMemo } from 'react';
+  import { decodeBase64 } from '../lib/utils/base64';
+  import { useSolanaWallets } from '@privy-io/react-auth';
+  
+  export function useActionSolanaWalletAdapter(
+    rpcUrlOrConnection: string | Connection,
+  ) {
+    const { wallets } = useSolanaWallets();
+    const wallet = wallets[0];
+  
+    const finalConnection = useMemo(() => {
+      return typeof rpcUrlOrConnection === 'string'
+        ? new Connection(rpcUrlOrConnection, 'confirmed')
+        : rpcUrlOrConnection;
+    }, [rpcUrlOrConnection]);
+  
+    const adapter = useMemo(() => {
+      function verifySignDataValidity(
+        data: string | SignMessageData,
+        opts: SignMessageVerificationOptions,
+      ) {
+        if (typeof data === 'string') {
+          // skip validation for string
+          return true;
+        }
+        const errors = verifySignMessageData(data, opts);
+        if (errors.length > 0) {
+          console.warn(
+            `[@dialectlabs/blinks] Sign message data verification error: ${errors.join(', ')}`,
+          );
+        }
+        return errors.length === 0;
+      }
+  
+      return new ActionConfig(finalConnection, {
+        connect: () => {
+          return Promise.resolve(wallet.address ?? null);
+        },
+        signTransaction: async (txData: string) => {
+          console.log('txData', txData);
+          try {
+            const tx = await wallet.sendTransaction(
+              VersionedTransaction.deserialize(decodeBase64(txData)),
+              finalConnection,
+            );
+            return { signature: tx };
+          } catch {
+            return { error: 'Signing failed.' };
+          }
+        },
+        signMessage: async (
+          data: string | SignMessageData,
+        ): Promise<
+          | { signature: string }
+          | {
+              error: string;
+            }
+        > => {
+          if (!wallet.signMessage || !wallet.address) {
+            return { error: 'Signing failed.' };
+          }
+          try {
+            // Optional data verification before signing
+            const isSignDataValid = verifySignDataValidity(data, {
+              expectedAddress: wallet.address,
+            });
+            if (!isSignDataValid) {
+              return { error: 'Signing failed.' };
+            }
+            const text =
+              typeof data === 'string' ? data : createSignMessageText(data);
+            const encoded = new TextEncoder().encode(text);
+            const signed = await wallet.signMessage(encoded);
+            const encodedSignature = bs58.encode(signed);
+            return { signature: encodedSignature };
+          } catch (e) {
+            return { error: 'Signing failed.' };
+          }
+        },
+      });
+    }, [finalConnection, wallet]);
+  
+    return { adapter };
+  }
---
+++ src/store/blinks/games.ts
@@ -0,0 +1,8 @@
+export const BLINKGAMES = {
+    snake:
+      'https://dial.to/?action=solana-action%3Ahttps%3A%2F%2Fsnakes.sendarcade.fun%2Fapi%2Factions%2Fgame%3F_brf%3Df722eb4a-297a-447b-aa1f-62f870b789fe%26_bin%3Dab63b0bf-abbd-4354-bb55-855309118e6a',
+    coinflip:
+      'https://dial.to/?action=solana-action%3Ahttps%3A%2F%2Fflip.sendarcade.fun%2Fapi%2Factions%2Fflip%3F_brf%3D9867785e-044d-4158-9b07-80a00db05052%26_bin%3D9f415adc-978d-4bfd-a5b8-66b0ca13f37e',
+    rockpaperscissors:
+      'https://dial.to/?action=solana-action%3Ahttps%3A%2F%2Frps.sendarcade.fun%2Fapi%2Factions%2Frps%3F_brf%3D5056cb65-8e5f-4812-bbfb-c887f555e91f%26_bin%3D9d908db2-5996-4c4c-9650-37530601e8e0',
+  } as const;
