f555013d96bf611b1da21242faa96d8827094caa
HaranK007 <shreeharan2003@gmail.com>
2025-01-30 20:06:33 +0530

    refactor: refactor route handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import { useEffect, useCallback } from 'react';
-import { Route, Routes } from 'react-router-dom';
 import { usePrivy, WalletWithMetadata } from '@privy-io/react-auth';
 import { useSolanaWallets } from '@privy-io/react-auth/solana';
-import PageTitle from './components/PageTitle';
-import Conversaction from './pages/Conversation';
-import MasterLayout from './layout/MasterLayout.tsx';
-import Onbording from './pages/Onbording';
-import WalletManagement from './pages/WalletManagement';
 import useAppState from './store/zustand/AppState';
-import Settings from './pages/Settings';
-import OnRamp from './pages/OnRamp';
 import useUser from './hooks/useUser';
 import useThemeManager from './store/zustand/ThemeManager.ts';
+import AppRoutes from './routes/appRoutes.tsx';
 // import { tokenGate } from './lib/solana/tokenGate';
 
 function App() {
@@ -49,23 +42,26 @@ function App() {
     console.log(await fetchSettings());
   }, [authenticated, ready, user]);
 
-  const initializeApp =  useCallback(async () => {
+  /**
+   * Callback function that handles createing embedded wallet and update user settings on user login
+   */
+  const initializeApp = useCallback(async () => {
     if (authenticated && ready) {
       const jwt = await getAccessToken();
-      console.log(jwt)
+      console.log(jwt);
       if (!jwt) {
         throw new Error('Failed to fetch access token.');
       }
       setAccessToken(jwt);
       await initializeWallet();
-      //TODO: get tire status here
+      //TODO: get user tire status here
       await updateUserSettings();
 
       if (wallets.length > 0) {
         setWallet(wallets[0]);
       }
     }
-  }, [authenticated, ready])
+  }, [authenticated, ready]);
 
   /**
    * Master UseEffect Run at app starts. Sets up app theme
@@ -75,72 +71,10 @@ function App() {
   }, []);
 
   useEffect(() => {
-    
-
     initializeApp();
-  }, []);
+  }, [initializeApp]);
 
-  const MemoizedAuthenticatedRoutes = useCallback(
-    () => (
-      <MasterLayout>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <PageTitle title="Home" />
-                <Conversaction />
-              </>
-            }
-          />
-          <Route
-            path="/c/:id"
-            element={
-              <>
-                <PageTitle title="Home" />
-                <Conversaction />
-              </>
-            }
-          />
-          <Route
-            path="/settings/configuration"
-            element={
-              <>
-                <PageTitle title="Settings" />
-                <Settings />
-              </>
-            }
-          />
-          <Route
-            path="/wallet"
-            element={
-              <>
-                <PageTitle title="Wallet Management" />
-                <WalletManagement />
-              </>
-            }
-          />
-          <Route
-            path="/onramp"
-            element={
-              <>
-                <PageTitle title="On Ramp" />
-                <OnRamp />
-              </>
-            }
-          />
-        </Routes>
-      </MasterLayout>
-    ),
-    [],
-  );
-  const MemoizedUnauthenticatedRoutes = useCallback(() => <Onbording />, []);
-
-  return authenticated && ready ? (
-    <MemoizedAuthenticatedRoutes />
-  ) : (
-    <MemoizedUnauthenticatedRoutes />
-  );
+  return <AppRoutes isAuthenticated={authenticated && ready} />;
 }
 
 export default App;
