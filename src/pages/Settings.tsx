b5425eeb1a40df2a0640ef5c7952edc7c7fb450e
Toastx <85751209+toastx@users.noreply.github.com>
2025-02-18 00:45:01 +0530

    fix: :lipstick: add X(back) to settings (#268)

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -2,15 +2,25 @@
  * Component to display configurable settings to the user
  */
 import useThemeManager from '../models/ThemeManager.ts';
-import { Sun, Moon } from 'lucide-react';
+import { Sun, Moon, X } from 'lucide-react';
 import { motion } from 'framer-motion';
 
 const Settings: React.FC = () => {
   const { theme, setTheme } = useThemeManager();
+  const goBack = () => {
+    window.history.back(); // Navigates back to the previous page
+  };
 
   return (
     <div className=" h-screen p-4 bg-background animate-in fade-in-0 duration-300">
       <div className=" relative bg-sec_background rounded-lg p-4 top-12 sm:top-2">
+        {/* Close button */}
+        <button 
+          className="absolute top-2 right-2 text-textColor hover:text-red-500" 
+          onClick={goBack}
+        >
+          <X size={20} />
+        </button>
         <h1 className="font-bold text-xl dark:text-purple-300">
           APP CONFIGURATION:
         </h1>
