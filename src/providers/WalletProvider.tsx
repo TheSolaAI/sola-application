6e6c5e64080d1c553946f7c26849e48129c343f9
Creator <shreeharan2003@gmail.com>
2025-04-29 23:42:41 +0530

    fix: :children_crossing: disable phantom embedded wallet in mobile (#449)

--- src/providers/WalletProvider.tsx
+++ src/providers/WalletProvider.tsx
@@ -3,20 +3,37 @@
 import React, { useEffect } from 'react';
 import { useSolanaWallets } from '@privy-io/react-auth/solana';
 import { useWalletHandler } from '@/store/WalletHandler';
+import useIsMobile from '@/utils/isMobile';
 
 export const WalletProvider: React.FC<{
   children: React.ReactNode;
   isAuthenticated: boolean;
 }> = ({ children, isAuthenticated }) => {
-  const { ready, wallets } = useSolanaWallets();
+  /*
+   * Global states
+   */
   const setWallets = useWalletHandler((state) => state.setWallets);
   const initWalletManager = useWalletHandler(
     (state) => state.initWalletManager
   );
+  const initPhantomWallet = useWalletHandler(
+    (state) => state.initPhantomWallet
+  );
+
+  /*
+   * Hooks
+   */
+  const isMobile = useIsMobile();
+
+  const { ready, wallets } = useSolanaWallets();
 
   useEffect(() => {
+    const initializePhantomEmbeddedWallet = async () => {
+      await initPhantomWallet();
+    };
     if (ready && isAuthenticated) {
       setWallets(wallets);
+      if (!isMobile) initializePhantomEmbeddedWallet();
       initWalletManager();
     }
   }, [ready, isAuthenticated, initWalletManager]);
